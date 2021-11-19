d0c1b8089debfb0d9936094d6860d5d5a00e6c66
vignesh prabhu <vignesh.prabhu70@gmail.com>
2021-11-19 19:11:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,28 +5,19 @@ import request from "./request";
 import { useState } from "react";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   const [isLargeRow, setIsLargeRow] = useState(true);
   console.log("hi");
   return (
-<<<<<<< HEAD
     <div className="app">
-      <Navbar />
-      <Banner />
-      <Row title="NETFLIX originals" fetchURL={request.trending} />
-      <Row title="Trending" fetchURL={request.netFlixOriginals} />
-      <Row title="Thrillers" fetchURL={request.trending} />
-=======
-    <div>
-<<<<<<< HEAD
-      hikjhbkjbk
-=======
-      achante achar
->>>>>>> c174386c7d3b642c1de929cfbacfbccbf1fba5b6
-      <Row title="netflix originals" fetchURL={request.trending} />
-      <Row title="trending" fetchURL={request.trending} />
-      <Row title="thrillers" fetchURL={request.trending} />
->>>>>>> ae1cef956de9506bd5f68a18e1440d36c90d3355
+      <Router>
+        <Navbar />
+        <Banner />
+        <Row title="NETFLIX originals" fetchURL={request.trending} />
+        <Row title="Trending" fetchURL={request.netFlixOriginals} />
+        <Row title="Thrillers" fetchURL={request.trending} />
+      </Router>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,50 @@
 import React from "react";
-import "Navbar.css";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
-  return <div className="nav">navbar</div>;
+  const [show, handleShow] = useState(true);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY < 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+  }, []);
+  return (
+    <div className={`nav ${!show && "nav_black"}`}>
+      <div className="nav_part1">
+        <img
+          className="nav__logo"
+          src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
+          alt="netflix logo"
+        ></img>
+        <Link className="nav_link" to="/">
+          Home
+        </Link>
+        <Link className="nav_link" to="/">
+          TV Shows
+        </Link>
+        <Link className="nav_link" to="/">
+          Movies
+        </Link>
+        <Link className="nav_link" to="/">
+          Recently added
+        </Link>
+        <Link className="nav_link" to="/">
+          My List
+        </Link>
+      </div>
+      <div className="nav_part2">
+        <img
+          className="nav__logo2"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="netflix logo"
+        ></img>
+      </div>
+    </div>
+  );
 }
